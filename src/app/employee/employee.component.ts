e6fb8d8529b7f9002f75d35e9d935bdb4c8895d0
MD NADEEM SARWAR <mdnadeem.mn289@gmail.com>
2023-10-06 00:10:12 +0530

    All code for  EmpManagementApp

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,35 @@
+import { Component,EventEmitter, OnInit,Input, Output } from '@angular/core';
+import { Employee } from '../models/employee.model';
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+   @Input() employee !: Employee;
+   @Output() onRemoveEmployee = new EventEmitter<number>();
+   @Output() onEditEmployee = new EventEmitter<number>();
+  constructor() {
+    this.employee = {
+      firstname : '',
+      lastname : '',
+      birthday : '',
+      gender : '',
+      education : '',
+      company : '',
+      jobExperience : 0,
+      salary : 0,
+      profile : '',
+    };
+   }
+  ngOnInit(): void {
+    console.log(this.employee);
+  }
+  deleteEmployeeClicked(){
+    this.onRemoveEmployee.emit(this.employee.id);
+  }
+   editEmployeeClicked(){
+    this.onEditEmployee.emit(this.employee.id);
+  } 
+
+}
